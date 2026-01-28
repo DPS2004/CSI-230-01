ab2fafaea7947f2ac23eaf6cc5d7e1230f1485d1
DPS2004 <dpsophinos@gmail.com>
2026-01-28 15:09:37 -0500

    Process management 1

---
+++ week2/pm4.ps1
@@ -0,0 +1,9 @@
+﻿$chromes = Get-Process | Where-Object{$_.Name -ilike "*chrome*"}
+$chromeCount = ($chromes | Measure-Object).Count
+if($chromeCount -gt 0){
+    Write-Host "Chrome is running, shutting it down"
+    Stop-Process $chromes
+}else{
+    Write-Host "Starting Chrome"
+    Start-Process "C:\Program Files\Google\Chrome\Application\chrome.exe" "Champlain.edu"
+}
