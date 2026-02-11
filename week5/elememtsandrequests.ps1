75c94d054431a2f5a5a97d86ea51846a928947e2
DPS2004 <dpsophinos@gmail.com>
2026-02-11 13:13:12 -0500

    Week 5 in class assignment

---
+++ week5/elememtsandrequests.ps1
@@ -0,0 +1,12 @@
+﻿$page = Invoke-WebRequest -TimeoutSec 10 http://10.0.17.42/ToBeScraped.html
+
+#$page.Links | Select-Object -Property outerText, href
+
+#$h2s = $page.ParsedHtml.body.getElementsByTagName("h2") | Select-Object -Property outerText
+#$h2s
+
+$div1s = $page.ParsedHtml.body.getElementsByTagName("div") | where { `
+    $_.getAttributeNode("class").Value -ilike "div-1" `
+} | Select-Object -Property innerText
+
+$div1s
