6359ee0918b2d6677d3eb519902f42dde0ecc16e
DPS2004 <dpsophinos@gmail.com>
2026-04-27 14:39:41 -0400

    Reading tables assignment done

---
+++ week13/readTables.bash
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+# This is the link we will scrape
+link="10.0.17.6/Assignment.html"
+
+# get it with curl and tell curl not to give errors
+fullPage=$(curl -sL "$link")
+
+# Utilizing xmlstarlet tool to extract table from the page
+table1=$(echo "$fullPage" | \
+xmlstarlet format --html --recover 2>/dev/null | \
+xmlstarlet select --template --copy-of \
+"(//html//body//table)[1]//tr//td")
+
+table1=$(echo "$table1" | sed 's/<\/tr>/\n/g' | \
+                     sed -e 's/&amp;//g' | \
+                     sed -e 's/<tr>//g' | \
+                     sed -e 's/<td[^>]*>//g' | \
+                     sed -e 's/<\/td>/;/g' | \
+                     sed -e 's/<[/\]\{0,1\}a[^>]*>//g' | \
+                     sed -e 's/<[/\]\{0,1\}nobr>//g')
+#echo "$table1"
+
+table2=$(echo "$fullPage" | \
+xmlstarlet format --html --recover 2>/dev/null | \
+xmlstarlet select --template --copy-of \
+"(//html//body//table)[2]//tr//td")
+
+table2=$(echo "$table2" | sed 's/<\/tr>/\n/g' | \
+                     sed -e 's/&amp;//g' | \
+                     sed -e 's/<tr>//g' | \
+                     sed -e 's/<td[^>]*>//g' | \
+                     sed -e 's/<\/td>/;/g' | \
+                     sed -e 's/<[/\]\{0,1\}a[^>]*>//g' | \
+                     sed -e 's/<[/\]\{0,1\}nobr>//g')
+#echo "$table1"
+#echo "$table2"
+
+#echo "$table1" | cut -d';' -f1
+
+
+justSemicolons="${table1//[^;]}"
+tableSize="${#justSemicolons}"
+
+for ((i=1; i<=$tableSize; i+=2)); do
+    pressure=$(echo "$table2" | cut -d';' -f$i)
+    temp=$(echo "$table1" | cut -d';' -f$i)
+    date=$(echo "$table1" | cut -d';' -f$((i + 1)))
+    echo "$pressure $temp $date"
+
+
+done
+
+
+
+
+
